8fd2720ff3e462f445898578967d1ff3b2eaf423
Administrator <Administrator@192.168.0.198>
2019-11-06 11:18:41 +0900

    fff

--- miniproject/src/kosta/mvc/model/util/LocationCode.java
+++ miniproject/src/kosta/mvc/model/util/LocationCode.java
@@ -18,9 +18,8 @@ public class LocationCode {
 		// 2개의 ~.properties 파일 로딩
 		try {
 
-			proFile.load(new FileInputStream(new File("src/kosta/mvc/model/util/locationInfo.properties")));
+			proFile.load(new FileInputStream(new File("miniproject/src/kosta/mvc/model/util/locationInfo.properties")));
 
-			Class.forName(proFile.getProperty("driverName"));
 
 		} catch (Exception e) {
 			e.printStackTrace();
